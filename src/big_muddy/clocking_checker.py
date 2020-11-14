318cdd51bb1f01787f72852529fa044f729856a5
Willy Mills <orwonthe@gmail.com>
2020-11-14 09:38:30 -0700

    Document ClockingChecker

--- src/big_muddy/clocking_checker.py
+++ src/big_muddy/clocking_checker.py
@@ -2,6 +2,20 @@ from signal_exception import SignalException
 
 
 class ClockingChecker:
+    """
+    Ensure clock signals travel through the entire hardware daisy chain.
+
+    The two clock signals, shift and load, are each on a GPIO output pin.
+    Those travel through the daisy chain (for robustness in opposite direction as data travels).
+    They each return to a pair of GPIO input pins.
+    If the daisy chain is intact, without disconnect cables or other hardware faults,
+    then the value on the input pin should match the value just placed on the output pin.
+    In normal operations a failure of clock integrity should raise an exception.
+    However, when debugging the hardware it is very useful to ignore the integrity
+    and keep the clock pulses running. The ClockingChecker is a singleton object
+    that does the integrity check normally, but can be turned off.
+
+    """
     # Default for whether to raise clocking exceptions.
     # Normally true for actual operations.
     # Sometimes handy to set to false when debugging hardware.
@@ -15,8 +29,10 @@ class ClockingChecker:
     def expect_false(value):
         if value and ClockingChecker.__raise_exception:
             raise SignalException("Clock stuck high")
+        return not value
 
     @staticmethod
     def expect_true(value):
         if not value and ClockingChecker.__raise_exception:
             raise SignalException("Clock stuck low")
+        return value

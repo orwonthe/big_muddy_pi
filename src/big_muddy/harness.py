12907177ece588e11cb98430ae259f347ff4a928
Willy Mills <orwonthe@gmail.com>
2024-09-14 16:00:24 -0600

    Add tracer test

--- src/big_muddy/harness.py
+++ src/big_muddy/harness.py
@@ -10,7 +10,7 @@ from data_probe import data_probe
 from duration_testing import duration_testing
 from flow_testing import flow_testing
 from load_testing import load_testing
-from shift_clock_testing import shift_clock_testing
+from shift_clock_testing import shift_clock_testing, shift_clock_tracing
 from wide_client_connector import wide_client_connector_test
 
 COMMAND_OPTIONS = [
@@ -76,6 +76,14 @@ def shifter(check):
     """Endless shift clocking"""
     ClockingChecker.set_raise_clock_exceptions(check)
     shift_clock_testing()
+
+@harness.command()
+@add_click_options(COMMAND_OPTIONS)
+def tracer(check):
+    """Endless shift clocking"""
+    ClockingChecker.set_raise_clock_exceptions(check)
+    while True:
+        shift_clock_tracing()
 
 
 @harness.command()
--- src/big_muddy/shift_clock_testing.py
+++ src/big_muddy/shift_clock_testing.py
@@ -1,3 +1,5 @@
+import time
+
 from big_muddy_io import BigMuddyIO
 
 
@@ -6,3 +8,12 @@ def shift_clock_testing():
     print('Shift clock testing: endless shift clocking')
     while True:
         big_muddy_io.shifting.pulse()
+
+def shift_clock_tracing():
+    big_muddy_io = BigMuddyIO.system()
+    print('Shift clock tracing: endless shift clocking')
+    while True:
+        big_muddy_io.shifting.output.write(1)
+        time.sleep(0.01)
+        big_muddy_io.shifting.output.write(0)
+        time.sleep(0.01)
